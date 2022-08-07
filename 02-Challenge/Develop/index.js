da753be337761836519cbefd73e8a5cb338dd89c
Adrian Firmalan <afirmalan@yahoo.com>
2022-08-07 11:45:39 -0700

    added input to index, added readme layout to markdowns.

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,13 +1,52 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
+const fs = require ('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: "input",
+        name: "title",
+        message: "What is your prohect title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is your project description?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "how do you install it?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How do you use it?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose the following licenses.",
+        choices: ["MIT", "IBM", "Apache", "Other"]
+
+    }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer.prompt(questions)
+        .then(data => {
+            console.log(data)
+            fs.writeFileSync("./output/README.md",generateMarkdown(data))
+        })
+}
 
 // Function call to initialize app
 init();
